a00da177381f676b704af552f710fcfd5c3a67a6
Christian Dawley <christian@art-box.us>
2018-04-07 09:10:07 -0400

    fixed assets

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+    // object containing crystal game
+const gameC = {
+
+    // constructor/prototype for crystal objects!
+    Crystal: function (number, worth) {
+    this.name = "crystal" + number;
+    this.value = worth;
+
+},
+
+    Goal: 0,
+    Score: 0,
+};
